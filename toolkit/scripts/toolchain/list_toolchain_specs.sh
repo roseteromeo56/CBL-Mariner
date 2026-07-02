0b05d6b8cb1ce5efb9324a92134e80ca609616ed
datadog-bits <263423550+datadog-bits@users.noreply.github.com>
2026-07-02 13:14:37 +0000

    Quote path vars in toolchain specs script

--- toolkit/scripts/toolchain/list_toolchain_specs.sh
+++ toolkit/scripts/toolchain/list_toolchain_specs.sh
@@ -14,7 +14,7 @@ OUTPUT_FILE=$2
 # In both cases, the first entry is the actual spec name to extract.
 # The below sed command will extract every spec name that follows the above pattern and place it in $OUTPUT_FILE.
 # `sort -u` is for eliminating duplicates- we don't actually care about the order
-sed -nE 's/^\s*build_rpm_in_chroot_no_install\s+([A-Za-z0-9_-]+).*$/\1/pgm' $TOOLCHAIN_BUILD_FILE | sort -u > $OUTPUT_FILE
+sed -nE 's/^\s*build_rpm_in_chroot_no_install\s+([A-Za-z0-9_-]+).*$/\1/pgm' "$TOOLCHAIN_BUILD_FILE" | sort -u > "$OUTPUT_FILE"
 
 # Special case to add msopenjdk-11 RPM which is downloaded instead of built
-echo "msopenjdk-11" >> $OUTPUT_FILE
+echo "msopenjdk-11" >> "$OUTPUT_FILE"
