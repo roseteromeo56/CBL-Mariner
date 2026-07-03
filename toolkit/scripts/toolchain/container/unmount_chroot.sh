700a7ac3c03621cb14aeb9b0e4d30618cce2b56b
datadog-bits <263423550+datadog-bits@users.noreply.github.com>
2026-07-03 12:11:54 +0000

    Quote LFS expansions in unmount_chroot.sh

--- toolkit/scripts/toolchain/container/unmount_chroot.sh
+++ toolkit/scripts/toolchain/container/unmount_chroot.sh
@@ -6,12 +6,12 @@ if [[ -z "$LFS" ]]; then
     echo "Must define LFS in environment" 1>&2
     exit 1
 fi
-echo LFS root is: $LFS
+echo "LFS root is: $LFS"
 
-umount -v $LFS/dev/pts
-umount -v $LFS/dev
-umount -v $LFS/run
-umount -v $LFS/proc
-umount -v $LFS/sys
-rm -v $LFS/dev/console
-rm -v $LFS/dev/null
+umount -v "${LFS}/dev/pts"
+umount -v "${LFS}/dev"
+umount -v "${LFS}/run"
+umount -v "${LFS}/proc"
+umount -v "${LFS}/sys"
+rm -v "${LFS}/dev/console"
+rm -v "${LFS}/dev/null"
