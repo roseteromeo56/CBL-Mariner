c3a920bb2689914e85c07ab34038eb1d9f715b8e
datadog-bits <263423550+datadog-bits@users.noreply.github.com>
2026-07-09 14:07:34 +0000

    Quote tarball arg in influxdb script

--- SPECS/influxdb/generate_source_tarball.sh
+++ SPECS/influxdb/generate_source_tarball.sh
@@ -86,7 +86,7 @@ NAME_VER="$PKG_NAME-$PKG_VERSION"
 VENDOR_TARBALL="$OUT_FOLDER/$NAME_VER-vendor.tar.gz"
 
 echo "Unpacking source tarball..."
-tar -xf $SRC_TARBALL
+tar -xf -- "$SRC_TARBALL"
 
 echo "Vendor go modules..."
 cd $NAME_VER
