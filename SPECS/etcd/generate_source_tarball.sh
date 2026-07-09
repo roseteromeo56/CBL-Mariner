768de3103d3628f32e5891a42995eebb1d1ec933
datadog-bits <263423550+datadog-bits@users.noreply.github.com>
2026-07-09 13:22:28 +0000

    Quote src tarball expansion in etcd script

--- SPECS/etcd/generate_source_tarball.sh
+++ SPECS/etcd/generate_source_tarball.sh
@@ -85,7 +85,7 @@ NAME_VER="$PKG_NAME-$PKG_VERSION"
 VENDOR_TARBALL="$OUT_FOLDER/$NAME_VER-vendor.tar.gz"
 
 echo "Unpacking source tarball..."
-tar -xf $SRC_TARBALL
+tar -xf "$SRC_TARBALL"
 
 cd "$NAME_VER"
 echo "Get vendored modules"
