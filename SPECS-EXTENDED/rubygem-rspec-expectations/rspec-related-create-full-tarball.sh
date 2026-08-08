c6df28fd0e83c574c3b0c37fb87b936e2f87ff1b
datadog-bits <263423550+datadog-bits@users.noreply.github.com>
2026-08-08 13:33:00 +0000

    Quote rspec tarball helper variables

--- SPECS-EXTENDED/rubygem-rspec-expectations/rspec-related-create-full-tarball.sh
+++ SPECS-EXTENDED/rubygem-rspec-expectations/rspec-related-create-full-tarball.sh
@@ -12,16 +12,16 @@ set -e
 CURRDIR=$(pwd)
 
 TMPDIRPATH=$(mktemp -d /var/tmp/rspec-tar-XXXXXX)
-pushd $TMPDIRPATH
+pushd "$TMPDIRPATH"
 
-git clone https://github.com/rspec/$1.git
-pushd $1
-git reset --hard v$2
+git clone "https://github.com/rspec/$1.git"
+pushd "$1"
+git reset --hard "v$2"
 popd
 
-ln -sf $1 $1-$2
-tar czf ${CURRDIR}/rubygem-$1-$2-full.tar.gz $1-$2/./
+ln -sf "$1" "$1-$2"
+tar czf "${CURRDIR}/rubygem-$1-$2-full.tar.gz" "$1-$2/./"
 
 popd
 
-rm -rf $TMPDIRPATH
+rm -rf "$TMPDIRPATH"
