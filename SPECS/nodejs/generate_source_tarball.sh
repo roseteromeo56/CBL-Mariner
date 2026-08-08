c24df3fdc8898610695a1bd53fe53eea566e5c1d
datadog-bits <263423550+datadog-bits@users.noreply.github.com>
2026-08-08 13:32:19 +0000

    Quote nodejs tarball generator paths

--- SPECS/nodejs/generate_source_tarball.sh
+++ SPECS/nodejs/generate_source_tarball.sh
@@ -27,8 +27,8 @@ PARAMS=""
 while (( "$#" )); do
     case "$1" in
         --srcTarball)
-        if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
-            SRC_TARBALL=$2
+        if [ -n "$2" ] && [ "${2:0:1}" != "-" ]; then
+            SRC_TARBALL="$2"
             shift 2
         else
             echo "Error: Argument for $1 is missing" >&2
@@ -36,8 +36,8 @@ while (( "$#" )); do
         fi
         ;;
         --outFolder)
-        if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
-            OUT_FOLDER=$2
+        if [ -n "$2" ] && [ "${2:0:1}" != "-" ]; then
+            OUT_FOLDER="$2"
             shift 2
         else
             echo "Error: Argument for $1 is missing" >&2
@@ -45,8 +45,8 @@ while (( "$#" )); do
         fi
         ;;
         --pkgVersion)
-        if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
-            PKG_VERSION=$2
+        if [ -n "$2" ] && [ "${2:0:1}" != "-" ]; then
+            PKG_VERSION="$2"
             shift 2
         else
             echo "Error: Argument for $1 is missing" >&2
@@ -77,31 +77,31 @@ echo "-- create temp folder"
 tmpdir=$(mktemp -d)
 function cleanup {
     echo "+++ cleanup -> remove $tmpdir"
-    rm -rf $tmpdir
+    rm -rf "$tmpdir"
 }
 trap cleanup EXIT
 
-pushd $tmpdir > /dev/null
+pushd "$tmpdir" > /dev/null
 
 namever="node-v${PKG_VERSION}"
 
-if [[ -n $SRC_TARBALL ]]; then
+if [[ -n "$SRC_TARBALL" ]]; then
     upstream_tarball_name="$SRC_TARBALL"
-    clean_tarball_name="$OUT_FOLDER/$(basename $SRC_TARBALL)"
+    clean_tarball_name="$OUT_FOLDER/$(basename "$SRC_TARBALL")"
 else
     upstream_tarball_name="${namever}.tar.xz"
     clean_tarball_name="$OUT_FOLDER/${namever}-clean.tar.xz"
     download_url="https://nodejs.org/download/release/v${PKG_VERSION}/${upstream_tarball_name}"
 
     echo "Downloading upstream source tarball..."
-    curl -s -O $download_url
+    curl -s -O "$download_url"
 fi
 
 echo "Unpacking upstream source tarball..."
-tar -xf $upstream_tarball_name
+tar -xf "$upstream_tarball_name"
 
 echo "Removing bad vendored dependencies from source tree..."
-rm -rf ./$namever/deps/openssl/openssl
+rm -rf "./$namever/deps/openssl/openssl"
 
 # Create a reproducible tarball
 # Credit to https://reproducible-builds.org/docs/archives/ for instructions
@@ -111,7 +111,7 @@ echo "Repacking source tarball..."
 tar --sort=name --mtime="2021-11-10 00:00Z" \
     --owner=0 --group=0 --numeric-owner \
     --pax-option=exthdr.name=%d/PaxHeaders/%f,delete=atime,delete=ctime \
-    -cJf $clean_tarball_name ./$namever
+    -cJf "$clean_tarball_name" "./$namever"
 
 popd > /dev/null
 echo "Clean nodejs source tarball available at $clean_tarball_name"
